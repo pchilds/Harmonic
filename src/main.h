fa5573525a20e11fddacd582587a089354f2caf2
Paul Childs <pchilds@physics.org>
2012-07-18 11:10:32 +1000

    adjusted for restyled gtkplot package yet again

--- src/main.h
+++ src/main.h
@@ -27,8 +27,8 @@
 #	include <gtk/gtk.h>
 #	include <glib/gi18n.h>
 #	include <math.h>
-#	include <gtk2plot-1.0/gtkplotlinear.h>
-#	include <gtk2plot-1.0/gtkplotpolar.h>
+#	include <gtkplot-2.0/gtkplotlinear.h>
+#	include <gtkplot-2.0/gtkplotpolar.h>
 	G_BEGIN_DECLS
 #	define DZE 0.00001 /* divide by zero threshold */
 #	define NZE -0.00001 /* negative of this */
