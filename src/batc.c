e480d47fd51aac6c348214a35f7a188e15324c83
Paul Childs <pchilds@physics.org>
2012-11-08 10:17:52 +1100

    minor bugfix

--- src/batc.c
+++ src/batc.c
@@ -138,6 +138,7 @@ void upt(GtkWidget* widget, gpointer dta)
 		{
 			lbl=gtk_label_new(g_strdup((gchar*) (fls->data)));
 			gtk_table_attach(GTK_TABLE(cft), lbl, 1, 2, 1, 2, GTK_FILL|GTK_SHRINK|GTK_EXPAND, GTK_FILL|GTK_SHRINK|GTK_EXPAND, 2, 2);
+			gtk_widget_show(lbl);
 			conff=g_list_append(conff, (gpointer) lbl);
 			g_free(fls->data);
 			fls=(fls->next);
@@ -184,6 +185,7 @@ void upt(GtkWidget* widget, gpointer dta)
 			atk_object_add_relationship(atk_wgt, ATK_RELATION_CONTROLLER_FOR, atk_ll);
 			lbl=gtk_label_new(g_strdup((gchar*) (fls->data)));
 			gtk_table_attach(GTK_TABLE(cft), lbl, 1, 2, rw-1, rw, GTK_FILL|GTK_SHRINK|GTK_EXPAND, GTK_FILL|GTK_SHRINK|GTK_EXPAND, 2, 2);
+			gtk_widget_show(lbl);
 			conff=g_list_append(conff, (gpointer) lbl);
 			g_free(fls->data);
 			fls=(fls->next);
